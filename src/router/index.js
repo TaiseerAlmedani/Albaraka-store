040ccdccd3495d41a567f96fa7f982bbd1c99917
TAISEER ALMEDANI <taiseer.almedani@gmail.com>
2023-12-23 19:40:25 +0300

    claer comments from router index

--- src/router/index.js
+++ src/router/index.js
@@ -9,25 +9,16 @@ const routes = [
       {
         path: '',
         name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (Home-[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import('@/views/Home.vue'),
       },
       {
         path: '/about',
         name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (Home-[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import('@/views/About.vue'),
       },
       {
         path: '/products',
         name: 'products',
-        // route level code-splitting
-        // this generates a separate chunk (Home-[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import('@/views/Products.vue'),
       },
     ],
